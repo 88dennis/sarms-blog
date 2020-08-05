16c5fa2327f03dbb0a724d358b2f3396c7c666af
Dennis Sarmiento <dennissarmiento8080@gmail.com>
2020-08-04 22:36:37 -0400

    up

---
+++ src/build/precache-manifest.fd8dead7b17e4a4c04fc52acb7e2b527.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cbf9cefc40e4ef76ccc72d335ce62e9",
+    "url": "/my-blog-app-v1/index.html"
+  },
+  {
+    "revision": "c106b66704f0711117b7",
+    "url": "/my-blog-app-v1/static/css/main.95d19a23.chunk.css"
+  },
+  {
+    "revision": "499b3bedb1dad6bdc7df",
+    "url": "/my-blog-app-v1/static/js/2.d3708d39.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-blog-app-v1/static/js/2.d3708d39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c106b66704f0711117b7",
+    "url": "/my-blog-app-v1/static/js/main.f5e8c2fb.chunk.js"
+  },
+  {
+    "revision": "5cac4114d8b056e34524",
+    "url": "/my-blog-app-v1/static/js/runtime-main.5f8eb9ee.js"
+  }
+]);

9472cdb89d022531009b023bfaa5ba160edb0f0b
Dennis Sarmiento <dennissarmiento8080@gmail.com>
2020-08-04 23:07:16 -0400

    up

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ app.use(express.static(path.join(__dirname, '/build')));
 }
 
 // if (process.env.NODE_ENV === 'production') {
-//     const path = require('path');
 //     // console.log('YOU ARE IN THE PRODUCTION ENV')
 //     app.use('/static', express.static(path.join(__dirname, './build/static')));
 //     app.get('/', (req, res) => {
